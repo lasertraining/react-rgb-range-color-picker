9f070d3e2d384a1f68b416896261eb1e6fb6635f
tomasconnor <tomasconnor.cz@gmail.com>
2022-07-09 21:39:28 +0200

    Basic pointers logic added to Color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,7 +1,18 @@
-import { useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 
 const ColorPicker = ({ colorHSL }) => {
+  const [mouseIsDown, setMouseIsDown] = useState(false);
+
+  const [selectedPointer, setSelectedPointer] = useState("");
+
+  const [pointers, setPointers] = useState({
+    pointer1: { x: 407, y: 110 },
+    pointer2: { x: 358, y: 62 },
+    pointer3: { x: 464, y: 64 },
+    pointer4: { x: 423, y: 22 },
+  });
+
   const canvasRef = useRef(null);
 
   const width = 500;
@@ -27,17 +38,76 @@ const ColorPicker = ({ colorHSL }) => {
     context.fillRect(0, 0, width, height);
   }, [colorHSL]);
 
+  const handleMouseDown = (pointer) => {
+    setMouseIsDown(true);
+    setSelectedPointer(pointer);
+  };
+
+  const handleMouseMove = (e) => {
+    if (mouseIsDown) {
+      const x = e.nativeEvent.offsetX;
+      const y = e.nativeEvent.offsetY;
+
+      setPointers((prevState) => ({
+        ...prevState,
+        [selectedPointer]: { x, y },
+      }));
+    }
+  };
+
+  const handleMouseUp = () => {
+    setMouseIsDown(false);
+    setSelectedPointer("");
+  };
+
   return (
-    <Container>
+    <Container onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
       <canvas ref={canvasRef} width={width} height={height} />
+
+      <Pointer
+        top={pointers.pointer1.y}
+        left={pointers.pointer1.x}
+        onMouseDown={() => handleMouseDown("pointer1")}
+      />
+      <Pointer
+        top={pointers.pointer2.y}
+        left={pointers.pointer2.x}
+        onMouseDown={() => handleMouseDown("pointer2")}
+      />
+      <Pointer
+        top={pointers.pointer3.y}
+        left={pointers.pointer3.x}
+        onMouseDown={() => handleMouseDown("pointer3")}
+      />
+      <Pointer
+        top={pointers.pointer4.y}
+        left={pointers.pointer4.x}
+        onMouseDown={() => handleMouseDown("pointer4")}
+      />
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
+
   canvas {
     border-radius: 25px;
   }
+`;
+
+const Pointer = styled.span.attrs(({ top, left }) => ({
+  style: {
+    top: `${top}px`,
+    left: `${left}px`,
+  },
+}))`
+  position: absolute;
+  background-color: #ffffff;
+  width: 10px;
+  height: 10px;
+  border-radius: 50px;
+  cursor: pointer;
 `;
 
 export default ColorPicker;
