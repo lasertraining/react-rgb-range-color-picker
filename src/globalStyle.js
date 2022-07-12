c443dc970bc5042c7b08702791b927955e1e8702
tomasconnor <tomasconnor.cz@gmail.com>
2022-07-12 10:08:40 +0200

    Global styles via styled-components added

---
+++ src/globalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background-color: #000000;
+    color: #97adad;
+    font-size: 15px;
+    user-select: none;
+  }
+
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    -moz-appearance: textfield;
+  } 
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { GlobalStyle } from "./globalStyle";
 import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
